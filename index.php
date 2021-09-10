dc638973c6d103ec351465db6b58a6492f2e5f10
Zachary Sawyer <mountainsounds15@gmail.com>
2021-09-10 19:01:37 -0400

    created home page and basic setup

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="idth=device-width, initial-scale=1.0">
+  <title>Welcome to Moogle!</title>
+
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+  <div class="wrapper indexPage">
+
+    <div class="mainSection">
+
+      <div class="logoContainer">
+        <img id='wordLogo' src="assets/images/bearCrawlLogo.png">
+        <img id='bearLogo' src="assets/images/bearLogo.jpg">
+      </div>
+
+      <div class="searchContainer">
+        <form action="search.php" method="GET">
+          <input class="searchBox" type="text" name="term">
+          <input class="searchButton" type="submit" value="Search">
+        </form>
+      </div>
+
+    </div>
+
+  </div>
+</body>
+</html>
