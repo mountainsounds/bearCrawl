3547f7e4562727c4fabbf8e6a0ec9a309830b092
Zachary Sawyer <mountainsounds15@gmail.com>
2021-09-16 07:08:22 -0400

    Styled search page

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="idth=device-width, initial-scale=1.0">
-  <title>Welcome to Moogle!</title>
+
+  <title>Welcome to Bear Crawl!</title>
 
   <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
